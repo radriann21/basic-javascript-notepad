9c03cb364e21def30d658c98e44e79e20bb2c784
radriann21 <adrianrangelargu@gmail.com>
2024-09-17 13:14:36 -0400

    added basic localstorage function

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let notes = []
+  if (!localStorage.getItem('notes')) {
+    localStorage.setItem('notes', JSON.stringify(notes))
+  } else {
+    notes = JSON.parse(localStorage.getItem('notes'))
+  }
+
+  const desc = document.getElementById('description')
+  const form = document.getElementById('form')
+
+  form.addEventListener('submit', (evt) => {
+    evt.preventDefault()
+
+    const formData = new FormData(evt.target)
+    const noteObj = {
+      id: crypto.randomUUID()
+    }
+
+    formData.forEach((value, key) => {
+      noteObj[key] = value
+    })
+    notes.push(noteObj)
+    localStorage.setItem('notes', JSON.stringify(notes))
+  })
+})
+
+
+
 
