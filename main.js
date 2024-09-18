b5bb1abfd177cce7c9dd404ffbc6cb579c88e254
radriann21 <adrianrangelargu@gmail.com>
2024-09-18 11:44:45 -0400

    corrected delete logic

--- main.js
+++ main.js
@@ -3,14 +3,16 @@ import { setNote, renderNotes, writeCharacters } from "./notesFunctions.js"
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('form')
   const notesContainer = document.getElementById('notesContainer')
-  const notes = JSON.parse(localStorage.getItem('notes')) || []
+  let notes = JSON.parse(localStorage.getItem('notes')) || []
   
   const description = document.getElementById('description')
   const chars = document.getElementById('number')
 
   writeCharacters(chars, description)
   renderNotes(notes, notesContainer)
-  form.addEventListener('submit', (evt) => setNote(notes, evt, notesContainer))
+  form.addEventListener('submit', (evt) => {
+    setNote(notes, evt, notesContainer)
+  })
 })
 
 
--- notesFunctions.js
+++ notesFunctions.js
@@ -14,22 +14,40 @@ export function setNote(notes, evt, notesContainer) {
   renderNotes([noteObj], notesContainer)
 }
 
+export function deleteTask(noteId, notes, note) {
+  const newNotes = notes.filter(note => note.id !== noteId)
+  localStorage.setItem('notes', JSON.stringify(newNotes))
+  note.remove()
+}
+
 export function renderNotes(notes, notesContainer) {
   if (!Array.isArray(notes)) return 
+
   notes.forEach(note => {
     const li = document.createElement('li')
+    li.setAttribute('data-id', note.id)
+    li.classList.add('element')
     li.innerHTML = `
+        <button class="close" id="close">X</button>
         <h3>${note.title}</h3>
         <p>${note.desc}</p>
       `
     notesContainer.appendChild(li)
+  })
+
+  notesContainer.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('close')) {
+      const note = evt.target.parentNode
+      const noteId = note.getAttribute('data-id')
+      const notes = JSON.parse(localStorage.getItem('notes'))
+      deleteTask(noteId, notes, note)
+    }
   })
 }
 
 export function writeCharacters(chars, description) {
   description.addEventListener('input', (evt) => {
     let number = evt.target.value.length
-
     if (number <= 500) {
       chars.innerText = number
     } 
